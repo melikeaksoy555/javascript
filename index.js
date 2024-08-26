6d59835eefff70d868d3e7de2981dc3aab9ac159
melikeaksoy555 <melikeficici@hotmail.com>
2024-08-26 21:52:33 +0300

    use strict kullanımı

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var firstName = "Alperen";
+"use strict";
 
-console. log (firstName);
+// var firstName = "Alperen";
+
+// console. log (firstName);
 
 
 // harfler rakamlar, $ , _ sembolü kullanılabilir.
 // ilk harf rakam olamaz
+
+// message = "merhaba dünya";
+
+var message = "selam dünya";
+
+// browser -> window 
+// server -> global
+
+console.log(message);
+
+// console.log(window.message);
+
+// window.console.log("merhaba");
+
+// sloppy mode
+// strict mode
