70a5eb5d0283cffb5f80f74a7813ad6f8aab1a99
melikeaksoy555 <melikeficici@hotmail.com>
2024-08-28 22:07:42 +0300

    if-else if-else ile switch-case-default işlendi.

--- if-ve-else.js
+++ if-ve-else.js
@@ -3,16 +3,51 @@
 // if(myBoolean){
 //console.log("merhaba dünya");
 //}
-const prefersDarkMode = false;
-const prefersSolarizedMode = true;
 
-if (prefersDarkMode) {
-  console.log("Dark mode etkinleştirildi.");
-  document.body.style.background = "black";
-} else if (){
+//const prefersDarkMode = true;
+// const prefersSolarizedMode = true;
 
-} else {
-    console.log("Light mode etkinleştirildi.");
-    document.body.style.background = "white";
-  }
-  
+// if (prefersDarkMode) {
+// console.log("Dark mode etkinleştirildi.");
+// document.body.style.background = "black";
+// } else if (prefersSolarizedMode){
+// console.log("Solarized mode etkinleştirildi.");
+// document.body.style.background = "palegoldenrod";
+// } else {
+// console.log("Light mode etkinleştirildi.");
+// document.body.style.background = "white";
+// }
+
+// const colorMode = "dark";
+
+// const colorMode = "solarized";
+
+// const colorMode = "alakasız bişi";
+
+// if (colorMode === "dark") {
+  // console.log("Dark mode etkinleştirildi.");
+  // document.body.style.background = "black";
+// } else if (colorMode === "solarized") {
+  // console.log("Solarized mode etkinleştirildi.");
+  // document.body.style.background = "palegoldenrod";
+// } else {
+  // console.log("Light mode etkinleştirildi.");
+  // document.body.style.background = "white";
+// }
+
+const colorMode = "dark";
+
+switch (colorMode) {
+    case "solarized":
+        console.log("Solarized mode etkinleştirildi");
+        document.body.style.background = "palegoldenrod";
+        break;
+    case "dark":
+        console.log("Dark mode etkinleştirildi");
+        document.body.style.background = "black";
+        break;
+    default:
+        console.log("Light mode etkinleştirildi");
+        document.body.style.background ="white";
+}
+
