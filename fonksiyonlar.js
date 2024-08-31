1427185a9e13c870a4a1dc72bcfdcb9b1645d02b
melikeaksoy555 <melikeficici@hotmail.com>
2024-08-31 23:42:18 +0300

    fonksiyonlara girildi: ya yeni bir action yada yeni bir data

---
+++ fonksiyonlar.js
@@ -0,0 +1,36 @@
+
+// input -> yeni bir action gerçekleştirir
+// input -> yeni bir data döner
+
+// let number = 0;
+
+// function increment(){
+    // number += 1;
+    // console.log("sayi artirildi");
+// }
+
+// increment();
+
+// function increment(){
+    // return 5;
+// }
+
+// console.log(increment());
+
+// function onlaCarp(girilecekSayi){
+    // return girilecekSayi * 10;
+//}
+
+// console.log(onlaCarp(5));
+
+// function greeting(age, name){
+    // return `Merhaba ${name}, yasiniz: ${age}`;
+// }
+
+// console.log(greeting(15, "Ali"));
+
+function greeting(){
+    return `Merhaba nasılsın?`;
+}
+
+console.log(greeting());
