6c518c867e0e8e8a8dd03520eab2e0e3c4833d2b
melikeaksoy555 <melikeficici@hotmail.com>
2024-09-14 20:48:49 +0300

    map2-object öreneği yapıldı

--- map1-object.js
+++ map1-object.js
@@ -1,13 +1,57 @@
-const colorPalette = {
-    blueColor: "#00f",
-    orangeColor: "f60",
-    //sayHi: function () {
-    sayHi: () => {
-        console.log("Hi");
-    },
-    isOpen: true,
-    countColors: 2,
+// const colorPalette = {
+//     blueColor: "#00f",
+//     orangeColor: "f60",
+//     //sayHi: function () {
+//     sayHi: () => {
+//         console.log("Hi");
+//     },
+//     isOpen: true,
+//     countColors: 2,
 
-}
+// }
 
-console.log(colorPalette.countColors);
+// console.log(colorPalette.countColors);
+
+//primitive types
+//undefined, null, boolean, number, string, symbol
+
+//non-primitive types
+//object {}
+
+//primitive!lerin hepsi değer ile geçer.
+// const myNum = 42;
+// const secondNum = 42;
+
+// console.log(myNum ===secondNum);
+
+// //object type'şarda reference ile geçer
+// const myObj = {
+//     name: "test"
+// };
+// const secondObj = {
+//     name: "test"
+// };
+
+// //console.log(myObj === secondObj);
+// console.log(myObj.name === secondObj.name);
+
+const myNum = 42;
+const secondNum = 42;
+
+
+// const myObj = {};
+
+// const secondObj = myObj;
+
+// secondObj.a = 1;
+// console.log(myObj);
+// console.log(secondObj);
+
+const myObj = 5;
+
+let secondObj = myObj;
+
+secondObj = 6;
+
+console.log(myObj);
+console.log(secondObj);
