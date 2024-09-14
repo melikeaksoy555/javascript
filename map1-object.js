f575336411563a64af9e96602ae4e956e08ff382
melikeaksoy555 <melikeficici@hotmail.com>
2024-09-14 20:14:16 +0300

    map1-object örneği yapıldı

---
+++ map1-object.js
@@ -0,0 +1,13 @@
+const colorPalette = {
+    blueColor: "#00f",
+    orangeColor: "f60",
+    //sayHi: function () {
+    sayHi: () => {
+        console.log("Hi");
+    },
+    isOpen: true,
+    countColors: 2,
+
+}
+
+console.log(colorPalette.countColors);
