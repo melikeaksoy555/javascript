9aeb006fdcacfacb58ef4f3536a17a95f0a78396
melikeaksoy555 <melikeficici@hotmail.com>
2024-09-15 09:18:20 +0300

    map5-object-destructuring örneği yapıldı

--- map1-object.js
+++ map1-object.js
@@ -38,7 +38,6 @@
 // const myNum = 42;
 // const secondNum = 42;
 
-
 // // const myObj = {};
 
 // // const secondObj = myObj;
@@ -56,13 +55,46 @@
 // console.log(myObj);
 // console.log(secondObj);
 
-const colors = {
-    "blue color": "#f00",
-    orange: "#f60",
+// const color = "black";
+// const hexCode = "#000";
+
+// const colors = {
+//   "blue color": "#f00",
+//   orange: "#f60",
+//   [color]: hexCode,
+// };
+
+// colors.red = "#foo";
+// colors.red = "#f00";
+
+// //console.log(colors);
+// //console.log(colors["blue color"]);
+
+
+// // aşağıdaki fonskiyonda değer gelmedi. HATA NEDİR?
+// function getColor(key) {
+//   return colors[key];
+// }
+// getColor("orange");
+
+const user = {
+    firstName: "Alperen",
+    username: "alperenustunn",
+    email: "alperen@archisacademy.com",
+    details: {
+        title: "Developer"
+    }
 }
 
-colors.red = "#foo";
-colors.red = "#f00"
+//Object Destructuring
 
-    //console.log(colors);
-    console.log(colors["blue color"]);
+// const {firstName, email, details: {title} } = user;
+
+// function displayUser(){
+    function displayUser({firstName, email, details: {title} }){
+    //console.log(`User: ${user.firstName} - Email: ${user.email} - Title: ${user.details.title}`);
+    console.log(`User: ${firstName} - Email: ${email} - Title: ${title}`);
+}
+
+// displayUser();
+displayUser(user);
