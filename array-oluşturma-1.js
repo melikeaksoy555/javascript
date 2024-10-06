9a7550a467e13ef4d323f815f40f57bb6380bd4d
melikeaksoy555 <melikeficici@hotmail.com>
2024-10-06 09:54:32 +0300

    Array & Set TR - 1 - Array Oluşturma kodlarına bakıldı

---
+++ array-oluşturma-1.js
@@ -0,0 +1,53 @@
+// const todos = {};
+
+// const todos = [];
+
+// const todo1 = {
+//   text: "Javascript öğren",
+//   completed: false,
+// };
+
+// const todo2 = {
+//   text: "Node.js öğren",
+//   completed: true,
+// };
+
+// todos["birinci"] = todo1;
+// todos["ikinci"] = todo2;
+
+// console.log(todos);
+
+// todos[0] = todo1;
+// todos[1] = todo2;
+
+// console.log(todos);
+
+// console.log(todos[0]);
+
+// todos.push(todo1, todo2);
+
+// console.log(todos.length);
+
+// console.log(todos.length - 1);
+
+// console.log(todos);
+
+// const sonELemanIndexi = todos.length - 1;
+
+// console.log(todos[sonELemanIndexi]);
+
+const todos = [
+  {
+    text: "Javascript öğren",
+    completed: false,
+  },
+
+  {
+    text: "Node.js öğren",
+    completed: true,
+  }
+];
+
+todos.pop();
+
+console.log(todos);
