b14679e8125b76e7a4cce36e2fe87e06305480b2
melikeaksoy555 <melikeficici@hotmail.com>
2024-11-06 11:28:36 +0300

    spread örneği yapıldı

---
+++ spread.js
@@ -0,0 +1,14 @@
+const dinnerMenuIdeas = ["Lahmacun", "Hamburger"];
+
+// const allDinnerIdeas = dinnerMenuIdeas;
+
+const allDinnerIdeas = [...dinnerMenuIdeas];
+
+// const allDinnerIdeas = dinnerMenuIdeas.concat("Kebab", "Chicken");
+
+allDinnerIdeas.push("Pizza");
+
+// console.log(dinnerMenuIdeas);
+
+console.log(dinnerMenuIdeas, "Dinner IDEAS");
+console.log(allDinnerIdeas, " All Dinner IDEAS");
