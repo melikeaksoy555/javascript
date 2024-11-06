cfb8814f5069459844b594e94f6672675e51e01d
melikeaksoy555 <melikeficici@hotmail.com>
2024-11-06 14:58:13 +0300

    destructuring örneği oluşturuldu

---
+++ Destructuring.js
@@ -0,0 +1,22 @@
+const finalMenuItems = [
+    "American Cheeseburger",
+    "Southern Fried Chicken",
+    "Glazed Salmon"
+];
+
+// const [test1, second, third] = finalMenuItems;
+
+// console.log(test1, second, third);
+
+// let first = finalMenuItems[0];
+// let second = finalMenuItems[1];
+// let third = finalMenuItems[2];
+
+// console.log(first, second, third);
+
+const [winner, ...losers] = finalMenuItems;
+
+// console.log(winner);
+
+console.log(losers);
+
