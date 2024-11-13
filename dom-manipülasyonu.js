1dead6b329f69c14434059c1380c72cfbe3986be
melikeaksoy555 <melikeficici@hotmail.com>
2024-11-13 23:51:47 +0300

    DOM - 4 - Dinamik Olarak CSS Stili Ekleme

--- dom-manipülasyonu.js
+++ dom-manipülasyonu.js
@@ -1,10 +1,20 @@
-const newPost = document.createElement('div');
-const mainElement = document.querySelector("#main");
+// const newPost = document.createElement('div');
+// const mainElement = document.querySelector("#main");
 
-newPost.className = "top-post";
+// newPost.className = "top-post";
 
-newPost.innerHTML = "<strong>Bu yeni bir post</strong>";
+// newPost.innerHTML = "<strong>Bu yeni bir post</strong>";
 
-// mainElement.appendChild(newPost);
+// // mainElement.appendChild(newPost);
 
-mainElement.prepend(newPost);
+// mainElement.prepend(newPost);
+
+const firstPost = document.querySelector("#firstPost");
+
+const btn = document.createElement("button");
+btn.innerText = "Post'u göster";
+document.body.prepend(btn);
+
+btn.addEventListener("click", () => {
+    firstPost.classList.toggle("invisible");
+});
