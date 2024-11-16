3e91b84cc92765ef09d08a7cd724dd11438f2223
melikeaksoy555 <melikeficici@hotmail.com>
2024-11-17 00:25:13 +0300

    DOM - 5 - Event'ler örnekleri yapıldı

--- dom-manipülasyonu.js
+++ dom-manipülasyonu.js
@@ -9,12 +9,31 @@
 
 // mainElement.prepend(newPost);
 
+// const firstPost = document.querySelector("#firstPost");
+
+// const btn = document.createElement("button");
+// btn.innerText = "Post'u göster";
+// document.body.prepend(btn);
+
+// btn.addEventListener("click", () => {
+//     firstPost.classList.toggle("invisible");
+// });
+
 const firstPost = document.querySelector("#firstPost");
+const input = document.querySelector("#myInput");
 
-const btn = document.createElement("button");
-btn.innerText = "Post'u göster";
-document.body.prepend(btn);
-
-btn.addEventListener("click", () => {
-    firstPost.classList.toggle("invisible");
+// input.addEventListener("keyup", (event) => {
+    input.addEventListener("change", (event) => {
+    // console.log(event.target.value);
+    generatenewPostTitle(event.target.value);
 });
+
+firstPost.addEventListener("click", (event) => {
+    console.log(event.target);
+})
+
+function generatenewPostTitle (title) {
+    const newPostTitle = document.createElement("h2");
+    newPostTitle.textContent = title;
+    document.body.prepend(newPostTitle);
+}
